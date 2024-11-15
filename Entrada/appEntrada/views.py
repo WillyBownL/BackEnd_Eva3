dde34808d494448c23933feaa79a726fabcef9d0
Pukyx <151487540+Pukyx@users.noreply.github.com>
2024-11-14 23:39:10 -0300

    1
    html+view

--- Entrada/appEntrada/views.py
+++ Entrada/appEntrada/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,render
+from appCliente.models import Cliente
+from appConcierto.models import Concierto
+from appEntrada.models import Entrada
 
 # Create your views here.
 
 
 def MenuPrincipal(request):
     return render(request, 'appEntrada/index.html')
+
+def listadoEntrada(request):
+    entradas = Entrada.objects.all()
+    data = {'entradas': entradas}
+    return render(request, 'appEntrada/listadoEntrada.html')
+
+def detalleEntrada(request, id):
+    return render(request, 'appEntrada/detalleEntrada.html', {'id': id})
