2bfac9c627778cd683b3e3d25918e7253e00023f
Pukyx <151487540+Pukyx@users.noreply.github.com>
2024-11-14 23:54:17 -0300

    2
    j

--- Entrada/appEntrada/models.py
+++ Entrada/appEntrada/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from appCliente.models import Cliente
+from appConcierto.models import Concierto
 # Create your models here.
+
+class Entrada(models.Model):
+    id_entrada = models.AutoField(primary_key=True)
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    id_concierto = models.ForeignKey(Concierto, on_delete=models.CASCADE)
+    precio = models.IntegerField()
+    area_designada = models.CharField(max_length=50)
+    fecha_reserva = models.DateField()
