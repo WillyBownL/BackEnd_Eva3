6dcf3c22318ef84f2b6ab93e460edf42a10ad03d
Willy Bown Leyton <wbown6@gmail.com>
2024-11-15 03:44:42 -0300

    admin finalizado

--- Entrada/appEntrada/admin.py
+++ Entrada/appEntrada/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from appEntrada.models import Entrada
 
 # Register your models here.
+class EntradaAdmin(admin.ModelAdmin):
+    list_display = ['id_cliente','id_concierto','precio','area_designada','fecha_reserva']
 
-admin.site.register(Entrada)
+admin.site.register(Entrada, EntradaAdmin)
 
 # admin 123
