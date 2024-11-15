7cf44bcc8085435598ba526edfb30ba6f8a8b21c
Willy Bown Leyton <wbown6@gmail.com>
2024-11-15 03:27:34 -0300

    cambios
    asasdasd

--- Entrada/appEntrada/models.py
+++ Entrada/appEntrada/models.py
@@ -3,7 +3,7 @@ from appCliente.models import cliente  # Importa el modelo Cliente desde appClie
 from appConcierto.models import Concierto  # Importa el modelo Concierto desde appConcierto
 
 class Entrada(models.Model):
-    id_entrada = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     id_cliente = models.ForeignKey(cliente, on_delete=models.CASCADE)  # Referencia a appCliente.Cliente
     id_concierto = models.ForeignKey(Concierto, on_delete=models.CASCADE)  # Referencia a appConcierto.Concierto
     precio = models.IntegerField()
